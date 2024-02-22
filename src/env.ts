5965d302b8bca5547bde3e2fe8fc8db37945c441
kinsyudev <miguel@solarlabs.gg>
2024-02-22 19:18:24 +0100

    added t3 env

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+    server: {
+    },
+    client: {
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string().min(1),
+        NEXT_PUBLIC_BACKEND_URL: z.string().min(1),
+    },
+    // For Next.js >= 13.4.4, you only need to destructure client variables:
+    experimental__runtimeEnv: {
+        NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    }
+});
