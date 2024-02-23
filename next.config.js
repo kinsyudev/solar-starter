60778150d17e1285405a96173140bcacf7b89982
kinsyudev <miguel@solarlabs.gg>
2024-02-23 10:16:39 +0100

    added env check at build time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+import createJiti from "jiti";
+
+const jiti = createJiti(new URL(import.meta.url).pathname);
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./src/env");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  webpack: (config, context) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
